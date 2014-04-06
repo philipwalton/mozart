495bb6ad45fadff4bfe4831e993dd569c6296198
Philip Walton <philip@philipwalton.com>
2014-04-05 22:55:00 -0700

    Change Klass to Class.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var Klass = require('./lib/klass');
+var Class = require('./lib/klass');
 
-// Expose a factory function to create new Klass instances.
+// Expose a factory function to create new Class instances.
 module.exports = function(name, definition) {
-  return new Klass(name, definition).construct();
+  return new Class(name, definition).construct();
 };
--- test/klass.js
+++ test/klass.js
@@ -1,167 +1,167 @@
 var test = require('tape');
 var sinon = require('sinon');
-var Klass = require('../lib/klass');
+var Class = require('../lib/klass');
 
-test('Klass#constructor'
+test('Class#constructor'
   + ' accepts a definition as its only parameter.', function(t) {
 
   t.plan(3);
 
   var definition = function() {};
-  var klass = new Klass(definition);
+  var cls = new Class(definition);
 
-  t.equal(klass.definition, definition);
-  t.notOk(klass.Ctor.name);
-  t.notOk(klass.parentKlass);
+  t.equal(cls.definition, definition);
+  t.notOk(cls.Ctor.name);
+  t.notOk(cls.parentClass);
 });
 
-test('Klass#constructor'
+test('Class#constructor'
   + ' accepts a name and a definition.', function(t) {
 
   t.plan(3);
 
   var definition = function() {};
-  var klass = new Klass('Foo', definition);
+  var cls = new Class('Foo', definition);
 
-  t.equal(klass.definition, definition);
-  t.equal(klass.Ctor.name, 'Foo');
-  t.notOk(klass.parentKlass);
+  t.equal(cls.definition, definition);
+  t.equal(cls.Ctor.name, 'Foo');
+  t.notOk(cls.parentClass);
 });
 
-test('Klass#constructor'
-  + ' accepts a definition and parent Klass.', function(t) {
+test('Class#constructor'
+  + ' accepts a definition and parent Class.', function(t) {
 
   t.plan(3);
 
   var definition = function() {};
-  var parentKlass = new Klass(function() {});
-  var klass = new Klass(definition, parentKlass);
+  var parentClass = new Class(function() {});
+  var cls = new Class(definition, parentClass);
 
-  t.equal(klass.definition, definition);
-  t.notOk(klass.Ctor.name);
-  t.equal(klass.parentKlass, parentKlass);
+  t.equal(cls.definition, definition);
+  t.notOk(cls.Ctor.name);
+  t.equal(cls.parentClass, parentClass);
 });
 
-test('Klass#constructor'
-  + ' accepts a name, definition, and parent Klass.', function(t) {
+test('Class#constructor'
+  + ' accepts a name, definition, and parent Class.', function(t) {
 
   t.plan(3);
 
   var definition = function() {};
-  var parentKlass = new Klass(function() {});
-  var klass = new Klass('Bar', definition, parentKlass);
+  var parentClass = new Class(function() {});
+  var cls = new Class('Bar', definition, parentClass);
 
-  t.equal(klass.definition, definition);
-  t.equal(klass.Ctor.name, 'Bar');
-  t.equal(klass.parentKlass, parentKlass);
+  t.equal(cls.definition, definition);
+  t.equal(cls.Ctor.name, 'Bar');
+  t.equal(cls.parentClass, parentClass);
 });
 
-test('Klass#constructor'
+test('Class#constructor'
   + ' creates a constructor.', function(t) {
 
   t.plan(1);
 
-  var klass = new Klass(function() {});
+  var cls = new Class(function() {});
 
-  t.ok(klass.Ctor);
+  t.ok(cls.Ctor);
 });
 
-test('Klass#constructor'
-  + ' sets up the inheritance chain when given a parent Klass.', function(t) {
+test('Class#constructor'
+  + ' sets up the inheritance chain when given a parent Class.', function(t) {
 
   t.plan(5);
 
-  var parentKlass = new Klass(function() {});
-  var klass = new Klass(function() {}, parentKlass);
+  var parentClass = new Class(function() {});
+  var cls = new Class(function() {}, parentClass);
 
-  t.equal(klass.parentKlass, parentKlass);
-  t.equal(klass.Parent, parentKlass.Ctor);
-  t.equal(klass.Ctor.super_, parentKlass.Ctor); // Node style `super_`.
-  t.equal(klass.Ctor.prototype.super, parentKlass.Ctor.prototype);
-  t.equal(klass.protectedMethods.super, parentKlass.protectedMethods);
+  t.equal(cls.parentClass, parentClass);
+  t.equal(cls.Parent, parentClass.Ctor);
+  t.equal(cls.Ctor.super_, parentClass.Ctor); // Node style `super_`.
+  t.equal(cls.Ctor.prototype.super, parentClass.Ctor.prototype);
+  t.equal(cls.protectedMethods.super, parentClass.protectedMethods);
 });
 
-test('Klass#constructor'
+test('Class#constructor'
   + ' creates the protected and private key and method objects', function(t) {
 
   t.plan(17);
 
-  var parentKlass = new Klass(function() {});
-  var klass = new Klass(function() {}, parentKlass);
-  var unrelatedKlass = new Klass(function() {});
+  var parentClass = new Class(function() {});
+  var cls = new Class(function() {}, parentClass);
+  var unrelatedClass = new Class(function() {});
 
-  t.ok(parentKlass.protectedKey);
-  t.ok(parentKlass.protectedMethods);
-  t.ok(parentKlass.privateKey);
-  t.ok(parentKlass.privateMethods);
+  t.ok(parentClass.protectedKey);
+  t.ok(parentClass.protectedMethods);
+  t.ok(parentClass.privateKey);
+  t.ok(parentClass.privateMethods);
 
-  t.ok(klass.protectedKey);
-  t.ok(klass.protectedMethods);
-  t.ok(klass.privateKey);
-  t.ok(klass.privateMethods);
+  t.ok(cls.protectedKey);
+  t.ok(cls.protectedMethods);
+  t.ok(cls.privateKey);
+  t.ok(cls.privateMethods);
 
-  t.ok(unrelatedKlass.protectedKey);
-  t.ok(unrelatedKlass.protectedMethods);
-  t.ok(unrelatedKlass.privateKey);
-  t.ok(unrelatedKlass.privateMethods);
+  t.ok(unrelatedClass.protectedKey);
+  t.ok(unrelatedClass.protectedMethods);
+  t.ok(unrelatedClass.privateKey);
+  t.ok(unrelatedClass.privateMethods);
 
-  // Protected keys are shared within klass heirachies, but not
-  // between unrelated klass instances.
-  t.equal(klass.protectedKey, parentKlass.protectedKey);
-  t.notEqual(klass.protectedKey, unrelatedKlass.protectedKey);
-  t.notEqual(parentKlass.protectedKey, unrelatedKlass.protectedKey);
+  // Protected keys are shared within class heirachies, but not
+  // between unrelated class instances.
+  t.equal(cls.protectedKey, parentClass.protectedKey);
+  t.notEqual(cls.protectedKey, unrelatedClass.protectedKey);
+  t.notEqual(parentClass.protectedKey, unrelatedClass.protectedKey);
 
-  // Protected method objects have their parent klass protected
+  // Protected method objects have their parent class protected
   // method objects in their prototype chain.
   t.equal(
-    Object.getPrototypeOf(klass.protectedMethods),
-    parentKlass.protectedMethods
+    Object.getPrototypeOf(cls.protectedMethods),
+    parentClass.protectedMethods
   );
   t.notEqual(
-    Object.getPrototypeOf(unrelatedKlass.protectedMethods),
-    parentKlass.protectedMethods
+    Object.getPrototypeOf(unrelatedClass.protectedMethods),
+    parentClass.protectedMethods
   );
 });
 
-test('Klass#constructor'
+test('Class#constructor'
   + ' adds the subclass and final methods to the constructor', function(t) {
 
   t.plan(2);
 
-  var klass = new Klass(function() {});
+  var cls = new Class(function() {});
 
-  t.ok(klass.Ctor.subclass);
-  t.ok(klass.Ctor.final);
+  t.ok(cls.Ctor.subclass);
+  t.ok(cls.Ctor.final);
 });
 
-test('Klass#construct'
+test('Class#construct'
   + ' invokes the definition and returns the constructor', function(t) {
 
   t.plan(4);
 
   var definition = sinon.spy();
-  var klass = new Klass(definition);
-  var ctor = klass.construct();
+  var cls = new Class(definition);
+  var ctor = cls.construct();
 
-  t.equal(ctor, klass.Ctor);
+  t.equal(ctor, cls.Ctor);
   t.ok(definition.called);
-  t.ok(definition.calledOn(klass.Ctor));
+  t.ok(definition.calledOn(cls.Ctor));
   t.ok(definition.calledWith(
-    klass.Ctor.prototype,
-    klass.protectedKey,
-    klass.protectedMethods,
-    klass.privateKey,
-    klass.privateMethods
+    cls.Ctor.prototype,
+    cls.protectedKey,
+    cls.protectedMethods,
+    cls.privateKey,
+    cls.privateMethods
   ));
 });
 
-test('Klass#construct'
+test('Class#construct'
   + ' returns a constructor that can subclass itself.', function(t) {
 
   t.plan(2);
 
-  var klass = new Klass(function() {});
-  var Ctor = klass.construct();
+  var cls = new Class(function() {});
+  var Ctor = cls.construct();
   var SubCtor = Ctor.subclass(function() {});
   var subInst = new SubCtor();
 
@@ -169,14 +169,14 @@ test('Klass#construct'
   t.equal(SubCtor.super_, Ctor);
 });
 
-test('Klass#construct'
+test('Class#construct'
   + ' returns a constructor that can prevent further'
   + ' subclassing.', function(t) {
 
   t.plan(1);
 
-  var klass = new Klass(function() {});
-  var Ctor = klass.construct();
+  var cls = new Class(function() {});
+  var Ctor = cls.construct();
   var SubCtor = Ctor.subclass(function() {});
   SubCtor.final();
 
@@ -185,17 +185,17 @@ test('Klass#construct'
   });
 });
 
-test('Klass#construct'
+test('Class#construct'
   + ' returnes a constructor that, when invoked, will invoke the'
   + ' init method on the prototype.', function(t) {
 
   t.plan(1);
 
   var init = sinon.spy();
-  var klass = new Klass(function(proto) {
+  var cls = new Class(function(proto) {
     proto.init = init;
   });
-  var Ctor = klass.construct();
+  var Ctor = cls.construct();
 
   new Ctor('foo', 'bar');
   t.ok(init.calledWith('foo', 'bar'));
